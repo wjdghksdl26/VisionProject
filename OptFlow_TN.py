8d30f6140056c4e1142fb021b7f92097b722fc71
wjdghksdl26 <60918852+wjdghksdl26@users.noreply.github.com>
2020-11-26 17:58:39 +0900

    201126

--- OptFlow_TN.py
+++ OptFlow_TN.py
@@ -7,7 +7,7 @@ import time
 termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
 feature_params = dict(maxCorners=30, qualityLevel=0.3, minDistance=50, blockSize=5, useHarrisDetector=False)
 lk_params = dict(winSize=(5, 5), maxLevel=2, criteria=termination, minEigThreshold=1e-5)
-search_new_points = False
+search_new_points = True
 switch_regions = False
 
 ap = argparse.ArgumentParser()
@@ -23,7 +23,7 @@ else:
 class App:
     def __init__(self):
         self.vid = cv2.VideoCapture(video)
-        self.track_len = 20
+        self.track_len = 30
         self.detect_interval = 5
         self.tracks = []
         self.frame_idx = 1
@@ -38,14 +38,13 @@ class App:
         while True:
             t_start = time.time()
             ret, frame = self.vid.read()
-            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+            frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
             if not ret:
                 print("End of Video Stream!")
                 break
 
             frame = imutils.resize(frame, width=400)
-            blurred = cv2.GaussianBlur(frame, (9, 9), 0)
             print("Frame", self.frame_idx)
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             vis = frame.copy()
@@ -69,10 +68,10 @@ class App:
                         del tr[0]
 
                     new_tracks.append(tr)
-                    cv2.circle(blurred, (int(x), int(y)), 4, (0, 0, 255), -1)
+                    cv2.circle(frame, (int(x), int(y)), 4, (0, 0, 255), -1)
 
                 self.tracks = new_tracks
-                cv2.polylines(blurred, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
+                cv2.polylines(frame, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
 
 
             if self.frame_idx % self.detect_interval == 0 and self.new_pts_switch == True:
@@ -105,7 +104,7 @@ class App:
             self.frame_idx += 1
             self.prev_gray = frame_gray
 
-            cv2.imshow('frame', blurred)
+            cv2.imshow('frame', frame)
             k = cv2.waitKey(1) & 0xFF
 
             if k == ord("s"):
